19ec1b6079b162c18fe9708eb6d402eb8d5bffc6
joseph <joseph.shih@104.com.tw>
2022-04-06 00:39:20 +0800

    使用 extraFunction  將每個更新 item 的動作改為 function

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,31 +12,12 @@ class GildedRose {
         	 if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                  continue;
              }
-        	 
-        	 item.sellIn = item.sellIn - 1;
-        	
             if (item.name.equals("Aged Brie")) {
-            	increaseQuality(item);
-            	if (item.sellIn < 0) {
-            		increaseQuality(item);
-            	}
+            	updateAgedBrieItem(item);
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            	increaseQuality(item);
-                 if (item.sellIn < 10) {
-                	 increaseQuality(item);
-                 }
-
-                 if (item.sellIn < 5) {
-                	 increaseQuality(item);
-                 }
-                 if (item.sellIn < 0) {
-                	 item.quality = item.quality - item.quality;
-             	}
+            	updateBackstageItem(item);
         	} else {
-        		decreaseQuality(item);
-        		if (item.sellIn < 0) {
-        			decreaseQuality(item);
-            	}
+        		updateItem(item);
             }
         }
     }
@@ -51,5 +32,36 @@ class GildedRose {
     	if (item.quality > 0) {
     		item.quality = item.quality - 1;
         }
+    }
+    
+    private void updateAgedBrieItem(Item item) {
+    	item.sellIn = item.sellIn - 1;
+    	increaseQuality(item);
+    	if (item.sellIn < 0) {
+    		increaseQuality(item);
+    	}
+    }
+    
+    private void updateBackstageItem(Item item) {
+    	item.sellIn = item.sellIn - 1;
+    	increaseQuality(item);
+        if (item.sellIn < 10) {
+       	 increaseQuality(item);
+        }
+
+        if (item.sellIn < 5) {
+       	 increaseQuality(item);
+        }
+        if (item.sellIn < 0) {
+       	 item.quality = 0;
+    	}
+    }
+    
+    private void updateItem(Item item){
+    	item.sellIn = item.sellIn - 1;
+    	decreaseQuality(item);
+		if (item.sellIn < 0) {
+			decreaseQuality(item);
+    	}
     }
 }
