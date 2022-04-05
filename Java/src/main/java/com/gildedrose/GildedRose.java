02c6e589b76ab1e159c81a8f6e7ac6292a4bd121
joseph <joseph.shih@104.com.tw>
2022-04-06 00:06:22 +0800

    將 Sulfuras, Hand of Ragnaros 的判斷移到最開始的位置，以移除重複程式碼

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+        	 if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                 continue;
+             }
+        	
             if (item.name.equals("Aged Brie")) {
             	if (item.quality < 50) {
                     item.quality = item.quality + 1;
@@ -28,20 +32,13 @@ class GildedRose {
                          item.quality = item.quality + 1;
                      }
                  }
-	             
         	} else {
-                
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+                	item.quality = item.quality - 1;
                 }
             }
-
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            } else {
-            	 item.sellIn = item.sellIn - 1;
-            }
+          
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
@@ -49,18 +46,12 @@ class GildedRose {
                         item.quality = item.quality + 1;
                     }
                 } else {
-                    
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     	item.quality = item.quality - item.quality;
                     } else {
                     	if (item.quality > 0) {
-                            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                               continue;
-                            } else {
-                            	 item.quality = item.quality - 1;
-                            }
+                    		item.quality = item.quality - 1;
                         }
-                        
                     }
                 }
             }
