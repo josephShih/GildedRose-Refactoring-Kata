0e2390721e548dca10129b5851014964e7e42725
joseph <joseph.shih@104.com.tw>
2022-04-06 00:52:44 +0800

    增減 quality 以及更新各 item 是 item 物件的工作，將增減 quality 及更新的 function 搬到 item 物件中

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,55 +13,12 @@ class GildedRose {
                  continue;
              }
             if (item.name.equals("Aged Brie")) {
-            	updateAgedBrieItem(item);
+            	item.updateAgedBrieItem();
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            	updateBackstageItem(item);
+            	item.updateBackstageItem();
         	} else {
-        		updateItem(item);
+        		item.updateItem();
             }
         }
-    }
-    
-    private void increaseQuality(Item item) {
-    	 if (item.quality < 50) {
-             item.quality = item.quality + 1;
-         }
-    }
-    
-    private void decreaseQuality(Item item) {
-    	if (item.quality > 0) {
-    		item.quality = item.quality - 1;
-        }
-    }
-    
-    private void updateAgedBrieItem(Item item) {
-    	item.sellIn = item.sellIn - 1;
-    	increaseQuality(item);
-    	if (item.sellIn < 0) {
-    		increaseQuality(item);
-    	}
-    }
-    
-    private void updateBackstageItem(Item item) {
-    	item.sellIn = item.sellIn - 1;
-    	increaseQuality(item);
-        if (item.sellIn < 10) {
-       	 increaseQuality(item);
-        }
-
-        if (item.sellIn < 5) {
-       	 increaseQuality(item);
-        }
-        if (item.sellIn < 0) {
-       	 item.quality = 0;
-    	}
-    }
-    
-    private void updateItem(Item item){
-    	item.sellIn = item.sellIn - 1;
-    	decreaseQuality(item);
-		if (item.sellIn < 0) {
-			decreaseQuality(item);
-    	}
     }
 }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,47 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+   
+   protected void increaseQuality() {
+  	 if (quality < 50) {
+           quality = quality + 1;
+       }
+  }
+  
+   protected void decreaseQuality() {
+  	if (quality > 0) {
+  		quality = quality - 1;
+      }
+  }
+   
+   protected void updateAgedBrieItem() {
+   	this.sellIn = this.sellIn - 1;
+   	increaseQuality();
+   	if (this.sellIn < 0) {
+   		increaseQuality();
+   	}
+   }
+   
+   protected void updateBackstageItem() {
+	this.sellIn = this.sellIn - 1;
+   	increaseQuality();
+       if (this.sellIn < 10) {
+       	increaseQuality();
+       }
+
+       if (this.sellIn < 5) {
+       	increaseQuality();
+       }
+       if (this.sellIn < 0) {
+      	 this.quality = 0;
+   	}
+   }
+   
+   protected void updateItem(){
+	this.sellIn = this.sellIn - 1;
+   	decreaseQuality();
+		if (this.sellIn < 0) {
+			decreaseQuality();
+   	}
+   }
 }
